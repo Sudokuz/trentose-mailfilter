c0385d7aa2e2374ec0eb8977555a4ad45a65d6c8
Sergey <sergio_lebedev@yahoo.it>
2015-12-02 20:03:34 +0100

    second sprint done

--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -1,5 +1,12 @@
-describe("MailModel", function() {
-  it("should..", function(){
+describe("MailModel init", function() {
+  it("should initialise mail database correctly", function(){
+      MailModel.init();
+      expect(msgs).toBe(MailModel.messages);
+  });
+    
+  it("should initialise rules database correctly", function(){
+      MailModel.init();
+      expect(rules).toBe(MailModel.rules);
   });
  
 });
